415c4afea42573b0a04b86f45b80147f3b432864
wisa <wisa.dev@gmail.com>
2019-11-18 18:40:41 +0900

    Initial Commit

---
+++ ToDo.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+export default class ToDo extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>I'm here </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({});
